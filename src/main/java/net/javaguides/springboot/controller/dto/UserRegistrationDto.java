d448492b0b6285d045098d0d7369029fea790754
Thang Tran <tranhuythang@gmail.com>
2023-03-15 09:34:04 -0700

    updated

--- src/main/java/net/javaguides/springboot/controller/dto/UserRegistrationDto.java
+++ src/main/java/net/javaguides/springboot/controller/dto/UserRegistrationDto.java
@@ -1,8 +1,7 @@
 package net.javaguides.springboot.controller.dto;
 
 public class UserRegistrationDto {
-//	private String firstName;
-//	private String lastName;
+
 	private String username;
 	private String password;
 	
@@ -10,33 +9,12 @@ public class UserRegistrationDto {
 		
 	}
 	
-//	public UserRegistrationDto(String firstName, String lastName, String email, String password) {
-//		super();
-//		this.firstName = firstName;
-//		this.lastName = lastName;
-//		this.username = email;
-//		this.password = password;
-//	}
-
-
 	public UserRegistrationDto(String username, String password) {
 		super();
 		this.username = username;
 		this.password = password;
 	}
 
-	//	public String getFirstName() {
-//		return firstName;
-//	}
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
-//	public String getLastName() {
-//		return lastName;
-//	}
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
 	public String getUsername() {
 		return username;
 	}
