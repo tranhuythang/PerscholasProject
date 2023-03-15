abed4a818c6035dba99cea6512f1f58804b19123
Thang Tran <tranhuythang@gmail.com>
2023-03-15 09:20:23 -0700

    fix some but not completely bugs

---
+++ readme.md
@@ -0,0 +1,18 @@
+This is a Web site for people to publish their reviews for books
+- Sign In/Up function
+- Non-registered users can READ reviews
+- Signed-in users can:
+    SUBMIT reviews for books
+    EDIT/DELETE submitted reviews
+
+Technologies used for this website:
+- Back-end:
+  Java and Spring Boot: Spring MVC, Spring Security
+  Hibernate
+  MySQL
+  Thymeleaf
+- Front-end:
+  HMTL, CSS
+  Javascript
+  Bootstrap 
+- Tools: IntelliJ, Maven, SL4J
--- src/main/resources/createTableNData.sql
+++ src/main/resources/createTableNData.sql
@@ -1,8 +1,6 @@
-
 drop database if exists testdb;
 create database testdb;
 use testdb;
-
 
 select * from book;
 insert into book(title, author, year) values
@@ -32,6 +30,7 @@ insert into user(username, password) values
 ('admin10', 'password'),
 ('admin11', 'password');
 
+select * from users_roles;
 insert into users_roles (user_id, role_id) values 
 (1, 1), 
 (2, 2),
@@ -47,18 +46,22 @@ insert into users_roles (user_id, role_id) values
 
 delete from review;
 insert into review(user_id, book_id, comment, rate, date) values
-(1, 1, 'comment 1 1', 3, '2023/02/22'),
-(2, 2, 'comment 2 2', 4, '2022/02/12'),
-(3, 3, 'comment 3 3', 5, '2021/05/23'),
-(4, 4, 'comment 4 4', 1, '2023/12/21'),
-(5, 5, 'comment 5 5', 2, '2023/06/22'),
-(6, 6, 'comment 6 6', 3, '2023/11/26'),
-(7, 7, 'comment 7 7', 4, '2023/04/27'),
-(8, 8, 'comment 8 8', 5, '2023/10/12'),
-(9, 9, 'comment 9 9', 3, '2023/09/02'),
-(10, 10, 'comment 10 10', 2, '202`/04/22'),
-(11, 11, 'comment 11 11', 1, '2020/05/07'),
-(1, 2, 'comment 1 2', 3, '2021/02/12'),
-(1, 3, 'comment 1 3', 4, '2023/08/04'),
-(1, 4, 'comment 1 4', 5, '2023/12/06'),
-(1, 5, 'comment 1 5', 1, '2023/11/08');
+(1, 1, 'Lorem ipsum dolor sit amet. Est tempore voluptatem non', 3, '2023/02/22'),
+(2, 2, 'reprehenderit voluptas eum aspernatur aliquid est eligendi similique', 4, '2022/02/12'),
+(3, 3, 'Et assumenda quidem et quam cumque qui excepturi suscipit et', 5, '2021/05/23'),
+(4, 4, 'magnam autem ad galisum vero aut voluptas odit. Et molestiae', 1, '2023/12/21'),
+(5, 5, 'voluptatem id quia quaerat ex architecto voluptatem. Qui', 2, '2023/06/22'),
+(6, 6, 'reprehenderit distinctio aut dolor voluptas ut minus rerum ut vitae', 3, '2023/11/26'),
+(7, 7, 'Qui fuga itaque in quas galisum ex quae repudiandae ut beatae', 4, '2023/04/27'),
+(8, 8, 'labore cum atque eius et placeat internos vel iusto illo. Non vitae', 5, '2023/10/12'),
+(9, 9, 'animi quo distinctio doloribus sed internos neque. Aut autem', 3, '2023/09/02'),
+(10, 10, 'similique est consectetur consequatur aut laudantium error et', 2, '202`/04/22'),
+(11, 11, 'sequi et eveniet esse et laborum distinctio sit illum illum qui amet', 1, '2020/05/07'),
+(1, 2, 'Sit delectus atque et nobis modi ut nihil rerum ut sunt minus sed', 3, '2021/02/12'),
+(1, 3, 'perspiciatis fugiat ut temporibus dolore sed quas enim aut fugit nisi', 4, '2023/08/04'),
+(1, 4, 'sit culpa quis eos reprehenderit consequuntur ut corporis', 5, '2023/12/06'),
+(1, 5, 'corrupti quas ut galisum repellat sed sint nulla? Eos consequatur', 1, '2023/11/08');
+
+select * from review;
+update user set password = '$2a$10$RPUVFjEBHkGHDf7yI9owNOvFzRPkEsl2cc/zYPrTYOAfjc6ptBHBC';
+delete from review where id=15;
